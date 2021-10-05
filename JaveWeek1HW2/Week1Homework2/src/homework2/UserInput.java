9f6e0802187ca3e784198a66dab99df2f4ef6676
Warunee Dinunzio <dinunziow@gmail.com>
2021-10-04 20:55:09 -0400

    finish homework 3

--- JaveWeek1HW2/Week1Homework2/src/homework2/UserInput.java
+++ JaveWeek1HW2/Week1Homework2/src/homework2/UserInput.java
@@ -1,10 +1,67 @@
 package homework2;
-
+import java.util.Scanner;
+import java.lang.Math;
 public class UserInput {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+/*
+ * User entered inputs (Scanner): 
 
+1. Write Fibonnaci series between the user entered start and end values
+
+2. Determine whether a user entered number is an Armstrong number: Armstrong number is a three digits number 
+that is an integer that the sum of the cubes of its digits is equal to the number itself. 
+For example, 371 = 3**3 + 7**7 + 1**1 = 371  
+
+3. Write a program to see if a user entered string is a palindrome or not
+*/
+// 1.
+// 2.
+	static boolean isArmstrong(int number) 
+	{
+		int temp, digits = 0, last = 0, sum = 0;
+		temp = number;
+		while ( temp > 0)
+		{
+			temp = temp/10;
+			digits++;
+		}
+		temp = number;
+		while(temp>0)   
+		{   
+		//determines the last digit from the number      
+		last = temp % 10;   
+		//calculates the power of a number up to digit times and add the resultant to the sum variable  
+		sum +=  (Math.pow(last, digits));   
+		//removes the last digit   
+		temp = temp/10;   
+		}  
+		//compares the sum with n  
+		if(number == sum)   
+		//returns if sum and n are equal  
+		return true;      
+		//returns false if sum and n are not equal  
+		else return false;   
+		} 
+	
+		//driver code  
+		public static void  main(String args[])     
+		{     
+		int num;   
+		Scanner input = new Scanner(System.in);  
+		System.out.print("Enter the number: ");  
+		//reads the limit from the user  
+		num = input.nextInt();  
+		if(isArmstrong(num))  
+		{  
+		System.out.print("Armstrong ");  
+		}  
+		else   
+		{  
+		System.out.print("Not Armstrong ");  
+	
 	}
+		System.exit(0);
+
+		}
 
 }
---
+++ Week1Homework3/src/week1Homework3/Calculator.java
@@ -0,0 +1,114 @@
+package week1Homework3;
+import java.util.Scanner;
+public class Calculator 
+{
+	/*
+	 * Write a program to create a calculator that can Add, Subtract, Multiply, and Divide 2 numbers entered by the user. Also, add an option to exit out from the program.
+
+  OPTIONS
+
+1. Add
+
+2. Subtract
+
+3. Multiply
+
+4. Divide
+
+5. Exit
+
+ 
+
+1. Write different methods for each module (add, subtract, multiply, and divide).
+
+2. Use switch/case to call the particular method.
+
+3. Take input inside the methods to perform the related operation.
+
+4. Return the answer and display it inside the main method.
+
+5. Display a default message (ex. "Invalid Entry, Try Again") if any number other than 1-5 is entered by the user
+
+
+	 */
+
+	public static void main(String[] args) 
+	{
+		int num1, num2, option;
+		
+		Scanner input = new Scanner(System.in);
+		
+		do 
+		{
+		
+		System.out.print("Enter your Options: 1 = Add, 2 = Subtract, 3 = Multiple, 4 = Divide, 0 = Exit :");
+		option = input.nextInt();
+		//int options = input.nextInt();
+		//input.close();
+		switch (option) 
+		{
+		case 1:
+			System.out.print("enter your first number: ");
+			num1 = input.nextInt();
+			System.out.print("enter your second number: ");
+			num2 = input.nextInt();
+			System.out.println("The result is " + add( num1,num2));
+			System.out.println();
+			break;
+		case 2:
+			System.out.print("enter your first number: ");
+			num1 = input.nextInt();
+			System.out.print("enter your second number: ");
+			num2 = input.nextInt();
+			System.out.println("The result is " + subtract( num1,num2));
+			break;
+		case 3:
+			System.out.print("enter your first number: ");
+			num1 = input.nextInt();
+			System.out.print("enter your second number: ");
+			num2 = input.nextInt();
+			System.out.println("The result is " + multiple( num1, num2));
+			break;
+		case 4:
+			System.out.print("enter your first number: ");
+			num1 = input.nextInt();
+			System.out.print("enter your second number: ");
+			num2 = input.nextInt();
+			System.out.println("The result is " + divide( num1, num2));
+			break;
+		case 0:
+			System.exit(0);
+			break;
+		default:
+			System.out.println("Invalid Entry, Try Again");
+		}
+		} while (option != 0);
+		
+	}
+	
+
+		public static int add(int n1, int n2)
+		{
+			int result = n1 + n2;
+			return result;
+		}
+		public static int subtract(int n1, int n2)
+		{
+			int result = n1 - n2;
+			return result;
+		}
+		public static int multiple(int n1, int n2)
+		{
+			int result = n1 * n2;
+			return result;
+		}
+		public static int divide(int n1, int n2)
+		{
+			int result = n1 / n2;
+			return result;
+		}
+		}
+		
+		
+	
+
